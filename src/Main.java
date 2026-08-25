30de67ecc0d34d72e9bdd8e3aa288940f7793687
oponjamesrey05-max <opon.jamesrey05@gmail.com>
2026-08-25 08:47:04 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import  java.util.Scanner;
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("===JAVA ACTIVITY PORTFOLIO===");
+    System.out.println("==Name: James");
+    System.out.println("==Section/Year: BSIT 2A");
+    System.out.println("=======CONTENTS=======");
+    System.out.println("1. Activity 1");
+    System.out.println("2. Activity 2");
+    System.out.println("==============================");
+    System.out.print("Enter Selection: ");
+    int select = sc.nextInt();
+
+
+
+
+
+}
